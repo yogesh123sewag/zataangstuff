aaebb175ccae964ede344d67280eb0864d3affb4
zataang.om <zataang.om@41296179-2956-0410-a65b-1163ca894e56>
2008-09-28 22:27:29 +0000

    added haptic keyboard trial code

---
+++ trunk/HapticKeyboard/Classes/HapticKeyboard.h
@@ -0,0 +1,16 @@
+//
+//  HapticKeyboard.h
+//  HapticKeyboard
+//
+
+#import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+
+NSBundle * myBundle;
+
+@interface HapticKeyboard : NSObject {
+}
+
+- (void) didInjectIntoProgram;
+
+@end
